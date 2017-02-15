e267fe7802aca3a8413a47e2987305fd7c977306
Erlend S <erl1sharpy@gmail.com>
2017-02-15 18:03:33 +0100

    Made selector, added some empty functions

--- src/Selector.java
+++ src/Selector.java
@@ -1,8 +1,30 @@
+import java.util.Dictionary;
+import com.google.gson.*;
+
 /**
  * Created by Erlend on 15.02.2017.
  */
 public class Selector {
 
+    public void action(String json_filename) {
+        //TODO
+    }
 
+    private void switch_major(String from, String to, int semesters) {
+        //TODO
+    }
+
+    private Course convert_to_course(Dictionary course) {
+        //TODO
+        return new Course("TDT4100");
+    }
+
+    private void get_course_from_db(String course) {
+        //TODO
+    }
+
+    private void get_plan_from_db(String plan) {
+        //TODO
+    }
 
 }
