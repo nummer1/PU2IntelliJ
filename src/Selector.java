808af49f940ac6006f5cfaabdd92dea0f822d720
Erlend S <erl1sharpy@gmail.com>
2017-02-17 12:32:43 +0100

    Added comments in Selector

--- src/Selector.java
+++ src/Selector.java
@@ -6,23 +6,29 @@ import com.google.gson.*;
  */
 public class Selector {
 
+    //This will be called by the model. It chooses the correct function based on the json from api.ai
     public void action(String json_filename) {
         //TODO
     }
 
+    //This would be the initial call to our main algorithm.
+    //It may have many differt helping functions which can be implemented when needed.
     private void switch_major(String from, String to, int semesters) {
         //TODO
     }
 
+    //Takes in information gotten from the database and puts it into a Course object.
     private Course convert_to_course(Dictionary course) {
         //TODO
         return new Course("TDT4100");
     }
 
+    //Gets courses from the database.
     private void get_course_from_db(String course) {
         //TODO
     }
 
+    //Gets information about the different plans for different majors.
     private void get_plan_from_db(String plan) {
         //TODO
     }
