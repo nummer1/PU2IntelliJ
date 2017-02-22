08d0388c8b1aedd2a29c1c6ddfe5d56b72700942
Andreas Flått <andreasflaatt@gmail.com>
2017-02-22 15:58:34 +0100

    Changed structure

--- java_tests/SelectorFirstYearTest.java
+++ java_tests/SelectorFirstYearTest.java
@@ -1,13 +1,14 @@
 import Algorithm.Selector;
 import junit.framework.TestCase;
 import java.util.ArrayList;
+import Algorithm.*;
 
 /**
  * Created by Erlend on 20.02.2017.
  */
 public class SelectorFirstYearTest extends TestCase {
 
-    ArrayList<ArrayList<Selector.Course>> plan;
+    ArrayList<ArrayList<Course>> plan;
     Selector sel = new Selector();
 
     public void setup() {
@@ -16,8 +17,8 @@ public class SelectorFirstYearTest extends TestCase {
 
     public void test_array() {
         setup();
-        ArrayList<Selector.Course> some_semester = this.plan.get(0);
-        Selector.Course some_course = some_semester.get(2);
+        ArrayList<Course> some_semester = this.plan.get(0);
+        Course some_course = some_semester.get(2);
         System.out.println(some_course.getCourse_id() + " " + some_course.getCourse_name() + " " + some_course.getExam_date());
         assertEquals("TMA4140", some_course.getCourse_id());
         assertEquals("Diskret matematikk", some_course.getCourse_name());
