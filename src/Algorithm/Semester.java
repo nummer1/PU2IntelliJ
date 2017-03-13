115c27fa230d48c392220564636efd20df9151aa
Nummer1 <AnonymNN2@gmail.com>
2017-03-13 13:49:27 +0100

    created functions in dbCom and some minor fixes in other classes

--- src/Algorithm/Semester.java
+++ src/Algorithm/Semester.java
@@ -2,13 +2,14 @@ package Algorithm;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by Erlend on 06.03.2017.
  */
 public class Semester {
 
-    private Collection courses = new ArrayList<Course>();
+    private Collection<Course> courses = new ArrayList<>();
     private boolean isSpring;
     private boolean isAutumn;
 
@@ -22,12 +23,13 @@ public class Semester {
         } else {
             throw new IllegalArgumentException("The season must be either spring or autumn");
         }
-
     }
 
     public void addCourse(Course course) {
         this.courses.add(course);
     }
+
+    public void addCourseList(List<Course> courseList) { this.courses = courseList; }
 
     public Collection getCourses() {
         return courses;
--- src/Algorithm/StudyPlan.java
+++ src/Algorithm/StudyPlan.java
@@ -1,14 +1,14 @@
 package Algorithm;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Erlend on 06.03.2017.
  */
 public class StudyPlan {
 
-    private Collection<Semester> semesters = new ArrayList<>();
+    private Map<Integer, Semester> semesters = new HashMap<>();
     private String name;
     private String major;
     private int score;
@@ -17,8 +17,8 @@ public class StudyPlan {
         this.name = name;
     }
 
-    public void addSemester(Semester semester) {
-        this.semesters.add(semester);
+    public void addSemester(Semester semester, Integer semesterNumber) {
+        this.semesters.put(semesterNumber, semester);
     }
 
     public void setScore(int score) {
