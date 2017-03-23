3e8beab49343735ddc9c3026efcaec2d22e192ca
hbjornoy <havard.bjornoy@gmail.com>
2017-03-23 11:20:57 +0100

    ops, deleted some excessive code in InputInterpreter

--- src/GUI/InputInterpreter.java
+++ src/GUI/InputInterpreter.java
@@ -61,49 +61,6 @@ public class InputInterpreter {
         } else {
             return "You have to write a question or request below"
         }
-
-        // I used setOnMouseClicked because you cant use ENTER, Maybe go for a textfield?
-        textArea.setOnMouseClicked(e -> {
-            if (!textArea.getText().trim().isEmpty()) {
-                AIResponse response = anna.GetResponse(textArea.getText());
-                String action = response.getResult().getAction();
-                boolean actionIncomplete = response.getResult().isActionIncomplete();
-                HashMap parameters = response.getResult().getParameters();
-                String speech = response.getResult().getFulfillment().getSpeech();
-
-                if (actionIncomplete) {
-                    textArea.setText("Anna: " + speech);
-                }
-                else {
-                    switch (action) {
-                        case "make.studyplan":
-                            String switchFromMajor = parameters.get("Switch-from-major").toString();
-                            String switchToMajor = parameters.get("Switch-to-major").toString();
-                            Object timeStudied = parameters.get("Switch-from-major"); //Maybe think about modifying @sys.duration and make it include semesters
-
-                            MidSection midSection = new MidSection();
-                            midSection.getCoursePlan().getChildren().clear(); // Clear previous studyplan if any.
-                            midSection.resetCounts(); // Reset counts for indexing courses.
-                            searchField.getSearchField().setVisible(true); // Set search-field visible.
-                            App.getLayout().setCenter(midSection.generateMidSection(switchFromMajor, switchToMajor));  //should have a field for years_studied/semester_studied
-                            App.getLayout().setAlignment(App.getLayout().getCenter(), Pos.CENTER);
-                            break;
-                        case "change.interface": // finn ut ka det heite
-                            //TODO:
-                            break;
-                        case "when is the exam og TDT4100":
-                            //TODO:
-                            break;
-                    }
-                    textArea.setText("Anna: " + speech);
-                }
-
-
-
-            }
-            else {
-                AlertBox.display("Feilmelding.", "Skriv noe til Anna");
-            }
-        });
+        return speech;
     }
 }
